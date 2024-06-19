399eb8dc30dcd23ba932a25b797a393c2cd168fe
Rohansolanki12 <rohansolanki054@gmail.com>
2024-06-19 10:59:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/Dil_Friends_12",
+    "https://github.com/Rohansolanki12/BrandrdXMusic2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
